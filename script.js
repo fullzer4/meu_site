a051b72411f4bb7c2c930ab747d99ea3d46a348d
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-04-15 15:24:23 -0300

    att

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const botao = document.getElementById('menu-botao');
 const ham = document.querySelectorAll('.menu-botao-ham');
-const menu = document.querySelectorAll("menu")
+const nave = document.getElementById('menu');
 let active = false;
 
 botao.onclick = ()=> {
@@ -8,11 +8,13 @@ botao.onclick = ()=> {
         ham[0].classList.add('hanima1');
         ham[1].classList.add('hanima2');
         ham[2].classList.add('hanima3');
+        nave.style.display = 'flex';
         active = true;
     } else {
         ham[0].classList.remove('hanima1');
         ham[1].classList.remove('hanima2');
         ham[2].classList.remove('hanima3');
+        nave.style.display = 'none';
         active = false;
     }
 }
