a6369074bb82a0e4bfc7f60036bba072e54ca56d
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-04-25 10:38:44 -0300

    att

---
+++ certificados/certificados.js
@@ -0,0 +1,20 @@
+const botao = document.getElementById('menu-botao');
+const ham = document.querySelectorAll('.menu-botao-ham');
+const nave = document.getElementById('menu');
+let active = false;
+
+botao.onclick = ()=> {
+    if(active == false){
+        ham[0].classList.add('hanima1');
+        ham[1].classList.add('hanima2');
+        ham[2].classList.add('hanima3');
+        nave.style.display = 'flex';
+        active = true;
+    } else {
+        ham[0].classList.remove('hanima1');
+        ham[1].classList.remove('hanima2');
+        ham[2].classList.remove('hanima3');
+        nave.style.display = 'none';
+        active = false;
+    }
+}
