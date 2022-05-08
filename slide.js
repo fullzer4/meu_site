514bafc45a49da87d7ac4b511a65571c31d7f295
fullzer4 <53914594+fullzer4@users.noreply.github.com>
2022-05-07 22:42:15 -0300

    att

--- slide.js
+++ slide.js
@@ -2,8 +2,7 @@ var counter = 1;
 setInterval(function(){
     document.getElementById("radio" + counter).checked = true;
     counter++;
-    if(counter > 3){
+    if(counter > 4){
         counter = 1;
     }
 }, 8000);
-
